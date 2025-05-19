0e385f3c2943526f602b4afda353980f872f5b76
Ilaria <maffiolailaria1@gmail.com>
2025-05-19 16:59:43 +0200

    [UPD] Wrap header in fragment in PostsPage component

--- src/Components/Posts/PostsPage.jsx
+++ src/Components/Posts/PostsPage.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import PostsList from './PostsList'
 const PostsPage = () => {
   return (
+    <>
+    <header>
+        <h1>I NOSTRI POST</h1>
+    </header>
     <div>
       <PostsList />
     </div>
+    </>
   )
 }
 
