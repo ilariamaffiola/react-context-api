466f6120b398edcb92a1aa18ecdbdf4f191588c8
Ilaria <maffiolailaria1@gmail.com>
2025-05-19 16:29:29 +0200

    [UPD] Fix positioning of useContext call in PostsList component

--- src/Components/Posts/PostsList.jsx
+++ src/Components/Posts/PostsList.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { useContext } from 'react'
 import PostListContext from '../../contexts/PostListContext'
 const PostsList = () => {
+    const {posts} = useContext(PostListContext);
   return (
     <div>
-      const {posts} = useContext(PostListContext);
       {posts.map((post) => (
         <div key={post.id}>
           <h2>{post.title}</h2>
